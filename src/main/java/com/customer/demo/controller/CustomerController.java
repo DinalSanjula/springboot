8caf595ab1591f6474959c66e0282b37b5302e8c
cclarkecampus <samila@c-clarke.com>
2026-05-17 20:56:16 +0530

    mmm

--- src/main/java/com/customer/demo/controller/CustomerController.java
+++ src/main/java/com/customer/demo/controller/CustomerController.java
@@ -1,10 +1,34 @@
 package com.customer.demo.controller;
 
+import com.customer.demo.models.Customer;
+import com.customer.demo.service.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
+
+/**
+ *
+ * @RestController is a combination of
+ *
+ * @contorler and @ResponseBody
+ *
+ */
 @RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomers();
+    }
 
 }
